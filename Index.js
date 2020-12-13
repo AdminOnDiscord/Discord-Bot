b83ebe7c2661619f3d34f90f5ba343e4fd841915
AdminOnDiscord <75921520+AdminOnDiscord@users.noreply.github.com>
2020-12-12 21:58:14 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -5,4 +5,4 @@ bot.registry.registerGroup('random' , 'Random');
 bot.registry.registerDefaults();  
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login('Nzg3NDg1MTk3MzkzMzMwMTc2.X9VovQ.w7ED_e1cxNo5oAusY9f2c13hHIA');
+bot.login(Bot Token);
